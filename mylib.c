4f6cb2676cc378db9f836d226e9e740ab94b5f10
Jacek Podkanski <ruby.object@googlemail.com>
2019-07-01 21:21:25 +0100

    hurray!!! the example window works

--- mylib.c
+++ mylib.c
@@ -10,26 +10,26 @@ int myMult(int x, int y, int (* cba)()) {
     return (res * 2);
 }
 
-/* static void activate (GtkApplication* app, gpointer user_data) */
-/* { */
-/*     GtkWidget *window; */
+static void activate (GtkApplication* app, gpointer user_data)
+{
+    GtkWidget *window;
 
-/*     window = gtk_application_window_new (app); */
-/*     gtk_window_set_title (GTK_WINDOW (window), "Window"); */
-/*     gtk_window_set_default_size (GTK_WINDOW (window), 200, 200); */
-/*     gtk_widget_show_all (window); */
-/* } */
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Window");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_widget_show_all (window);
+}
 
 int zzz ()
 {
     GtkApplication *app;
     int status = 0;
-    int argc = 0;
 
+    int argc = 0;
     char **argv = (char *[]){"hello", "world"};
 
     app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    //g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
     status = g_application_run (G_APPLICATION (app), argc, argv);
     g_object_unref (app);
 
